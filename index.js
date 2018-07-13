1cf9a5f6758ebd0c5401382c893a7e37e1b0697e
Alec Staszewski <astaszew@gmail.com>
2018-07-13 15:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object;
 }
 function deleteFromObjectByKey(object, key){
-  var clone = Object.assign({},object);
+  var clone = object;
   delete clone[key];
   return clone;
+
 }
